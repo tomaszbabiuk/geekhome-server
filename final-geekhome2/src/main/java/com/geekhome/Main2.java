86d6cc90df168eb35b3690d3f609dc398c4e1056
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2019-12-27 12:12:52 +0100

    adding sample rest servlet

--- final-geekhome2/src/main/java/com/geekhome/Main2.java
+++ final-geekhome2/src/main/java/com/geekhome/Main2.java
@@ -11,6 +11,8 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.PathResource;
 
+import org.glassfish.jersey.servlet.ServletContainer;
+
 import java.io.IOException;
 import java.nio.file.Paths;
 
@@ -21,21 +23,26 @@ public class Main2 {
         ResourceHandler rh0 = new ResourceHandler();
         rh0.setDirectoriesListed(false);
 
-        ContextHandler context2 = new ContextHandler();
-        context2.setContextPath("/");
-        context2.setBaseResource(new PathResource(Paths.get("src/main/resources/dist")));
-        context2.setHandler(rh0);
+        ContextHandler webContext = new ContextHandler();
+        webContext.setContextPath("/");
+        webContext.setBaseResource(new PathResource(Paths.get("src/main/resources/dist")));
+        webContext.setHandler(rh0);
 
-        ServletContextHandler context0 = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context0.setContextPath("/");
-        context0.addServlet(new ServletHolder(new HelloServlet()),"/*");
+        ServletContextHandler streamContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        streamContext.setContextPath("/stream");
+        streamContext.addServlet(new ServletHolder(new MySSEServlet()), "/*");
 
-        ServletContextHandler context1 = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context1.setContextPath("/talk");
-        context1.addServlet(new ServletHolder(new MySSEServlet()), "/*");
+        ServletContextHandler restContext =
+                new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        ServletHolder serHol = restContext.addServlet(ServletContainer.class, "/rest/*");
+        serHol.setInitOrder(1);
+        serHol.setInitParameter("jersey.config.server.provider.packages",
+                "com.geekhome.rest");
+
+
 
         ContextHandlerCollection contexts = new ContextHandlerCollection();
-        contexts.setHandlers(new Handler[] { context2, context1, context0 });
+        contexts.setHandlers(new Handler[] { webContext, streamContext, restContext });
 
 
         JettyHttpServer server = new JettyHttpServer(80, contexts);
