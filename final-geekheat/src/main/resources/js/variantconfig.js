4be49628a1864b26d78024faebdc27953a25216c
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2020-11-17 20:00:11 +0100

    goodbye maven, you served well

--- final-geekheat/src/main/resources/js/variantconfig.js
+++
@@ -1,8 +0,0 @@
-var CONFIG = {
-    HeatingCategoryEnabled: true,
-    HotWaterCategoryEnabled: true,
-    LocksCategoryEnabled: false,
-    LightsCategoryEnabled: false,
-    VentilationCategoryEnabled: false,
-    MonitoringCategoryEnabled: false
-}
