f379c8b208ae717bfc8cd821fa1b01fb1a5573de
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2021-10-10 19:11:29 +0100

    Improving bash scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd bin
-java -jar final-geekhome-all.jar -Dfile.encoding=UTF-8 -Dlog4j.configurationFile=log4j2.xml -Djava.library.path=/usr/lib/jni
+java -Dfile.encoding=UTF-8 -Dlog4j.configurationFile=./log4j2.xml -Djava.library.path=/usr/lib/jni -jar final-geekhome-all.jar
