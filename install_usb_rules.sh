477977700f0f15c971571924ca9e06b34a20ac35
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2021-10-10 20:41:41 +0100

    build scripts

--- install_usb_rules.sh
+++ install_usb_rules.sh
@@ -1,2 +1,2 @@
 cp scripts/50-geekhome-extafree.rules /etc/udev/rules.d/50-geekhome-extafree.rules
-cp scripts/etc/51-geekhome-1wire.rules /etc/udev/rules.d/51-geekhome-1wire.rules
+cp scripts/51-geekhome-1wire.rules /etc/udev/rules.d/51-geekhome-1wire.rules
