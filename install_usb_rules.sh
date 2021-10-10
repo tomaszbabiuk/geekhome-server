b5fd9ddff0dec8cf84d303a74ce5993bb1414748
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2021-10-10 20:40:42 +0100

    startup sctipts

--- install_usb_rules.sh
+++ install_usb_rules.sh
@@ -1,2 +1,2 @@
-cp scripts/etc/udev/rules.d/50-geekhome-extafree.rules /etc/udev/rules.d/50-geekhome-extafree.rules
-cp scripts/etc/udev/rules.d/51-geekhome-1wire.rules /etc/udev/rules.d/51-geekhome-extafree.rules
+cp scripts/50-geekhome-extafree.rules /etc/udev/rules.d/50-geekhome-extafree.rules
+cp scripts/etc/51-geekhome-1wire.rules /etc/udev/rules.d/51-geekhome-1wire.rules
