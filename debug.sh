1189857f5341dcc40a7bae7d499237b3700faf5d
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2021-10-11 16:52:56 +0200

    build scripts

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 cd /home/pi/geekhome
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=9000,server=y,suspend=y -Dfile.encoding=UTF-8 -Dlog4j.configurationFile=./log4j2.xml -Djava.library.path=/usr/lib/jni -jar final-geekhome-all.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:9000 -Dfile.encoding=UTF-8 -Dlog4j.configurationFile=./log4j2.xml -Djava.library.path=/usr/lib/jni -jar final-geekhome-all.jar
